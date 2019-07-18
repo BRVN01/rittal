b02c23bf21c9cd471e65c3a6d20f88e7007a7252
BRVN01 <39762901+BRVN01@users.noreply.github.com>
2019-07-18 12:17:23 -0300

    Create dicionariosRacks.py

---
+++ dicionariosRacks.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3.5
+
+dici_corredor_4_frio = {
+    'F38': 'cmcIIIVarValueStr.2.2',
+    'P38': 'cmcIIIVarValueStr.4.2',
+    'K38': 'cmcIIIVarValueStr.11.2',
+    'F35': 'cmcIIIVarValueStr.7.2',
+    'P35': 'cmcIIIVarValueStr.10.2',
+    'K35': 'cmcIIIVarValueStr.8.2'
+    }
+
+dici_corredor_4_quente = {
+    'K38': 'cmcIIIVarValueStr.3.2',
+    'K35': 'cmcIIIVarValueStr.9.2'
+    }
+
+dici_corredor_4_umidade = {
+    'F38': 'cmcIIIVarValueStr.2.12',
+    'P38': 'cmcIIIVarValueStr.4.11',
+    'F35': 'cmcIIIVarValueStr.7.12',
+    'P35': 'cmcIIIVarValueStr.10.12',
+    'K35': 'cmcIIIVarValueStr.8.11'
+    }
+
+dici_corredor_6_frio = {
+    'F54': 'cmcIIIVarValueStr.2.2',
+    'P54': 'cmcIIIVarValueStr.5.2',
+    'K54': 'cmcIIIVarValueStr.4.2',
+    'F51': 'cmcIIIVarValueStr.7.2',
+    'P51': 'cmcIIIVarValueStr.11.2',
+    'K51': 'cmcIIIVarValueStr.10.2'
+    }
+
+dici_corredor_6_quente = {
+    'K54': 'cmcIIIVarValueStr.3.2',
+    'K51': 'cmcIIIVarValueStr.9.2'
+    }
+
+dici_corredor_6_umidade = {
+    'F54': 'cmcIIIVarValueStr.2.12',
+    'P54': 'cmcIIIVarValueStr.5.11',
+    'F51': 'cmcIIIVarValueStr.7.12',
+    'P51': 'cmcIIIVarValueStr.11.12',
+    'K51': 'cmcIIIVarValueStr.10.12'
+    }
+
+dici_corredor_5_frio = {
+    'F46': 'cmcIIIVarValueStr.2.2',
+    'P46': 'cmcIIIVarValueStr.6.2',
+    'K46': 'cmcIIIVarValueStr.4.2',
+    'G43': 'cmcIIIVarValueStr.8.2',
+    'M43': 'cmcIIIVarValueStr.10.2',
+    'O43': 'cmcIIIVarValueStr.11.2'
+    }
+
+dici_corredor_5_quente = {
+    'K46': 'cmcIIIVarValueStr.5.2',
+    'K43': 'cmcIIIVarValueStr.9.2'
+    }
+
+dici_corredor_5_umidade = {
+    'F46': 'cmcIIIVarValueStr.2.12',
+    'P46': 'cmcIIIVarValueStr.6.12',
+    'K46': 'cmcIIIVarValueStr.4.12',
+    'G43': 'cmcIIIVarValueStr.8.12',
+    'M43': 'cmcIIIVarValueStr.10.11',
+    'O43': 'cmcIIIVarValueStr.11.11'
+    }
+
+ip = {
+    'cor4': '10.2.9.26',
+    'cor5': '10.2.9.27',
+    'cor6': '10.2.9.28'
+    }
